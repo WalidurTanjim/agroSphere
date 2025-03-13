3b7dc1ddb33230a2310e90345b015090ace15250
azim622 <fazleazim622@gmail.com>
2025-03-14 00:09:31 +0600

    added agroFram

---
+++ src/pages/Home/Home/AgroFarm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AgroFarm = () => {
+    return (
+        <div>
+            <h2 className='text-5xl font-bold'>HEllo bro</h2>
+        </div>
+    );
+};
+
+export default AgroFarm;
