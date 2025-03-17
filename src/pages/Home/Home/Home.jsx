ad9f70872ce1edb5de525e86e29a7acb78ce468c
azim622 <fazleazim622@gmail.com>
2025-03-17 22:30:02 +0600

    added community

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import NewsLatter from './NewsLatter';
 import VideoPlaylist from '../../../Components/VideoPlaylist';
 import AICropAdvisory from '../../../AI Intrigate/AICropAdvisory';
+import LatestForum from './LatestForum';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <VideoPlaylist></VideoPlaylist>
+            <LatestForum></LatestForum>
             <NewsLatter></NewsLatter>
           <AICropAdvisory></AICropAdvisory>
         </div>
