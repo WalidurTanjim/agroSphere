6beafab8966726ca01343d9fbc899623823d896a
SaikatAhmed78 <shaikatahmed78@gmail.com>
2025-03-17 23:06:47 +0600

    fix: properly integrate AI API with error handling and UI improvements

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import NewsLatter from './NewsLatter';
 import VideoPlaylist from '../../../Components/VideoPlaylist';
 import Trainers from './Trainers/Trainers';
+import AICropAdvisory from '../../../AI Intrigate/AICropAdvisory';
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
             <VideoPlaylist></VideoPlaylist>
             <Trainers />
             <NewsLatter></NewsLatter>
+            <AICropAdvisory></AICropAdvisory>
         </div>
     );
 };
