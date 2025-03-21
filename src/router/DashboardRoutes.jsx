731143f6c2780fc443aa6551df03015319e6370e
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-03-21 23:01:21 +0600

    added new routes on admin dashboard side nav

--- src/router/DashboardRoutes.jsx
+++ src/router/DashboardRoutes.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeft, Home, LayoutDashboard } from "lucide-react";
+import { ArrowDownToDot, ArrowLeft, Home, LayoutDashboard, Users, Video } from "lucide-react";
 import React, { useState } from "react"
 import { Link, useRouteError } from "react-router-dom";
 import logo from '../assets/logo.png'
@@ -8,7 +8,6 @@ import useAuth from "../hooks/useAuth";
 const DashboardRoutes = () => {
     const [isSideNavOpen, setIsSideNavOpen] = useState(false);
     const { user } = useAuth();
-    console.log(user?.photoURL);
     const { userRole } = useUserRole();
 
     return (
@@ -52,6 +51,30 @@ const DashboardRoutes = () => {
                                                 <div className="flex items-center self-center"><LayoutDashboard size={20} /></div>
 
                                                 <div className="flex w-full flex-1 flex-col items-start justify-center gap-0 overflow-hidden truncate text-sm">Admin Dashboard</div>
+                                            </Link>
+                                        </li>
+
+                                        <li className="px-3">
+                                            <Link to='/dashboard/vide-upload' className="flex items-center gap-3 rounded p-3 text-gray-400 transition-colors hover:bg-gray-100  hover:text-gray-600 active:bg-gray-100 focus:bg-gray-100 aria-[current=page]:bg-gray-100 aria-[current=page]:text-gray-400">
+                                                <div className="flex items-center self-center"><Video size={20} /></div>
+
+                                                <div className="flex w-full flex-1 flex-col items-start justify-center gap-0 overflow-hidden truncate text-sm">Video Upload</div>
+                                            </Link>
+                                        </li>
+
+                                        <li className="px-3">
+                                            <Link to='/dashboard/incomming-requests' className="flex items-center gap-3 rounded p-3 text-gray-400 transition-colors hover:bg-gray-100  hover:text-gray-600 active:bg-gray-100 focus:bg-gray-100 aria-[current=page]:bg-gray-100 aria-[current=page]:text-gray-400">
+                                                <div className="flex items-center self-center"><ArrowDownToDot size={20} /></div>
+
+                                                <div className="flex w-full flex-1 flex-col items-start justify-center gap-0 overflow-hidden truncate text-sm">Incomming Requests</div>
+                                            </Link>
+                                        </li>
+
+                                        <li className="px-3">
+                                            <Link to='/dashboard/all-users' className="flex items-center gap-3 rounded p-3 text-gray-400 transition-colors hover:bg-gray-100  hover:text-gray-600 active:bg-gray-100 focus:bg-gray-100 aria-[current=page]:bg-gray-100 aria-[current=page]:text-gray-400">
+                                                <div className="flex items-center self-center"><Users size={20} /></div>
+
+                                                <div className="flex w-full flex-1 flex-col items-start justify-center gap-0 overflow-hidden truncate text-sm">All Users</div>
                                             </Link>
                                         </li>
                                     </> :
