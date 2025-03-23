78ac50dbb41bb436cd925cd34a803993cf24d262
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-03-23 16:14:56 +0600

    change the format of main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,30 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 import "./index.css";
 
-import { RouterProvider } from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import router from "./router/Router";
-import { Toaster } from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
 import AuthProvider from "./context/AuthProvider";
-import { ThemeProvider } from "./context/ThemeProvider";
+import {ThemeProvider} from "./context/ThemeProvider";
 
 import {
-  QueryClient,
-  QueryClientProvider,
+    QueryClient,
+    QueryClientProvider,
 } from "@tanstack/react-query";
 
 // Create a client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-      <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-        <ThemeProvider>
-          <RouterProvider router={router} />
-          <Toaster />
-        </ThemeProvider>
-    </AuthProvider>
-      </QueryClientProvider>
-  </StrictMode>
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <ThemeProvider>
+                    <RouterProvider router={router}/>
+                    <Toaster/>
+                </ThemeProvider>
+            </AuthProvider>
+        </QueryClientProvider>
+    </StrictMode>
 );
