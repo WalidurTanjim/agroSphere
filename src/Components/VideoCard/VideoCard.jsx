d2619ffcb1f9284f6a38eb9873487ffa1d2f21b3
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-24 02:35:17 +0600

    Spinner component created and get all video now have to show

---
+++ src/Components/VideoCard/VideoCard.jsx
@@ -0,0 +1,9 @@
+const VideoCard = ({ video }) => {
+    return (
+        <div className="video-card group w-full p-2 border border-gray-200 rounded-lg hover:shadow-lg">
+            <h1>Video tutorial</h1>
+        </div>
+    );
+};
+
+export default VideoCard;
