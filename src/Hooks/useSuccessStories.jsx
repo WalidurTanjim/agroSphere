7bf57c426262dcdecc01d23934808ea05265042f
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-24 01:30:39 +0600

    useSuccessStories hook created to get all success stories

---
+++ src/Hooks/useSuccessStories.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
+
+const useSuccessStories = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { data: stories = [], isPending, isError, error, refetch } = useQuery({
+        queryKey: ['stories', axiosPublic],
+        queryFn: async() => {
+            try{
+                const res = await axiosPublic.get('/success-stories');
+                const data = await res.data;
+                if(data){
+                    return data || [];
+                }
+            }catch(err){
+                console.error(err);
+                return [];
+            }
+        }
+    })
+
+    return [ stories, isPending, isError, error, refetch ];
+};
+
+export default useSuccessStories;
