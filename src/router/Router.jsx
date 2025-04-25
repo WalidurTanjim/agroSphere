689093ca787ab610d4ae22bc5f8c75db427f39e6
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-25 18:13:46 +0600

    Seller profile done

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -30,6 +30,7 @@ import Market from "../pages/Market/Market.jsx";
 import ProductDetails from "../components/ProductDetails/ProductDetails.jsx";
 import IncommingRequests from "../pages/Dashboard/Admin/IncommingRequests/IncommingRequests.jsx";
 import Sellers from "../pages/Sellers/Sellers.jsx";
+import SellerProfile from "../components/Prodile/SellerProfile/SellerProfile.jsx";
 
 
 const router = createBrowserRouter([
@@ -101,6 +102,10 @@ const router = createBrowserRouter([
       {
         path: 'community/post-details/:id',
         element: <PostDetails />
+      },
+      {
+        path: 'seller-profile/:email',
+        element: <SellerProfile />
       }
     ]
   },{
