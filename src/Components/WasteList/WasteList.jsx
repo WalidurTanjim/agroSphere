ea95fefa3b05ebab08e50f7846e5858288f2c8e4
Abrar <hossainabrar470@gmail.com>
2025-05-01 22:05:11 +0600

    Farmer Dashboard Extra Two Routes

--- src/Components/WasteList/WasteList.jsx
+++ src/Components/WasteList/WasteList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BuyWasteModal from "../BuyWasteModal/BuyWasteModal";
+import DashboardRoutes from "../../router/DashboardRoutes";
 
 const WasteList = () => {
     const [wastes, setWastes] = useState([]);
@@ -20,22 +21,26 @@ const WasteList = () => {
     };
 
     return (
-        <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {wastes.map(waste => (
-                <div key={waste._id} className="border p-4 rounded-lg shadow">
-                    {waste.imageURL && <img src={waste.imageURL} alt="Waste" className="rounded w-full h-[200px]" />}
-                    <h2 className="text-xl font-bold pt-3">{waste.type}</h2>
-                    <p>Quantity: {waste.quantity}KG</p>
-                    <p>Days Stored: {waste.daysStored}Day</p>
-                    <button onClick={() => handleBuyNow(waste)} className="mt-3 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-                        Buy Now
-                    </button>
-                </div>
-            ))}
+        <div>
+            <DashboardRoutes></DashboardRoutes>
+            <h1 className="text-center text-2xl lg:text-3xl font-bold py-10">Waste Market</h1>
+            <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {wastes.map(waste => (
+                    <div key={waste._id} className="border p-4 rounded-lg shadow">
+                        {waste.imageURL && <img src={waste.imageURL} alt="Waste" className="rounded w-full h-[200px]" />}
+                        <h2 className="text-xl font-bold pt-3">{waste.type}</h2>
+                        <p>Quantity: {waste.quantity}KG</p>
+                        <p>Days Stored: {waste.daysStored}Day</p>
+                        <button onClick={() => handleBuyNow(waste)} className="mt-3 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+                            Buy Now
+                        </button>
+                    </div>
+                ))}
 
-            {selectedWaste && (
-                <BuyWasteModal waste={selectedWaste} onClose={handleCloseModal} />
-            )}
+                {selectedWaste && (
+                    <BuyWasteModal waste={selectedWaste} onClose={handleCloseModal} />
+                )}
+            </div>
         </div>
     );
 };
